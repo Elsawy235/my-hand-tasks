872e9b29726f016670afdff709fd034bbd639e66
Mahmoud Elsawy <74200665+Elsawy235@users.noreply.github.com>
2025-10-08 19:28:53 +0300

    Delete vm_windows directory

--- vm_windows/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vm_public_ip" {
-  value = azurerm_public_ip.public_ip.ip_address
-}
-
-output "vm_private_ip" {
-  value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
-}
